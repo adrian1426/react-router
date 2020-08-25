08bfa3d2f7ad276912b1c9fa1e1ae1b230c93a97
Adrian1426 <adrianhh1426@gmail.com>
2020-08-24 22:34:35 -0500

    diferentes formas de renderizar con Route

--- src/App.js
+++ src/App.js
@@ -8,8 +8,32 @@ function App() {
   return (
     <BrowserRouter>
       <Route path='/' exact component={Home} />
-      <Route path='/productos/' strict component={Productos} />
-      <Route path='/Clientes' sensitive component={Clientes} />
+
+      {/*
+       el método componente utiliza internamente el React.createElement
+       por lo tanto por cada renderizado react creará un nuevo componente
+       */}
+      <Route path='/componente' component={() => (<div>Hola amigos</div>)} />
+
+      {/*
+       React recomienda utilizar render, cuando los componentes son funcionales
+       para no crearlos varios veces con component
+       */}
+      <Route path='/productos' render={Productos} />
+
+      {/*
+       Siempre será renderizado
+       */}
+      <Route path='/clientes'>
+        {
+          (props) => {
+            console.log(props);
+            return (
+              <Clientes />
+            )
+          }
+        }
+      </Route>
     </BrowserRouter>
   );
 }
