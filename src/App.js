b9a34986da823edaf3caf15ed22402c7a2fdf022
Adrian1426 <adrianhh1426@gmail.com>
2020-08-24 22:02:52 -0500

    props disponibles en route, exact,estrict,sensitive

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/home';
 import Productos from './components/productos';
+import Clientes from './components/clientes';
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path='/home' component={Home} />
-      <Route path='/productos' component={Productos} />
-      <section>
-        <Route path='/productos' component={Productos} />
-      </section>
+      <Route path='/' exact component={Home} />
+      <Route path='/productos/' strict component={Productos} />
+      <Route path='/Clientes' sensitive component={Clientes} />
     </BrowserRouter>
   );
 }
